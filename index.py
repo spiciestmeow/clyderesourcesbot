d653a41d830646dcb7f4bb4d8e0b07d11916b1e2
spiciestmeow <clydebustamantex@gmail.com>
2026-04-02 18:19:40 +0800

    Update index.py

--- index.py
+++ index.py
@@ -14,15 +14,20 @@ tg_app = Application.builder().token(TOKEN).build()
 async def send_welcome_message(chat_id, first_name):
     """Simple async function to send the response"""
     keyboard = [
-        [InlineKeyboardButton("🛒      Steam Accounts      🛒", url="https://your-link.com")],
-        [InlineKeyboardButton("🔑      Learn & Guides      🔑", url="https://your-link.com")],
-        [InlineKeyboardButton("🔵      Official Discord      🔵", url="https://discord.gg/invite")],
-        [InlineKeyboardButton("🌐      Our Website      🌐", url="https://clyderesourcehub.short.gy/")],
-        [InlineKeyboardButton("🌟      Reviews & Feedback      🌟", url="https://your-link.com")],
-        [InlineKeyboardButton("📨      Contact & Advertise      📨", url="https://your-link.com")]
+        [
+            InlineKeyboardButton("Steam Accounts", url="https://clydehub.notion.site/d1402294d90683468aa1814447299e13?v=0c802294d90683e69030086dfb718034"),
+            InlineKeyboardButton("Learn & Guides", url="https://clydehub.notion.site/d1402294d90683468aa1814447299e13?v=21502294d9068345834508d28ffbe79e")
+        ],
+        [InlineKeyboardButton("Our Website", url="https://clyderesourcehub.short.gy/")],
+        [InlineKeyboardButton("Contact & Advertise", url="https://t.me/caydigitals")]
     ]
     
-    caption = f"👋 Hello, {first_name}!\n\nThis channel serves as the primary router for our project. ❤️"
+    caption = (
+    f"👋 **Hi there, {first_name}!**\n\n"
+    "You've found the heart of our project. This channel is designed "
+    "to help you navigate our ecosystem quickly and easily.\n\n"
+    "We're glad to have you here! Check out the buttons below to get started. 🌿"
+    )
     GIF_URL = "https://media3.giphy.com/media/v1.Y2lkPTc5MGI3NjExMm1kbGExNW12Z3ZpcjRtZmcwcjAxNmJ3YnA5NmRzMjQwNno2NGo2dSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/tkaDAjbZUmoH1a1Z2R/giphy.gif"
 
     await tg_app.bot.send_animation(
